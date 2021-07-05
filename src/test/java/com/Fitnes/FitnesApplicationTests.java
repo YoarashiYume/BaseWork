93ff5b1dd80d61f799aa5e846481ee3f6fee9dd9
YoarashiYume <fdn000@mail.ru>
2021-07-05 19:22:50 +0300

    init

---
+++ src/test/java/com/Fitnes/FitnesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fitnes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
