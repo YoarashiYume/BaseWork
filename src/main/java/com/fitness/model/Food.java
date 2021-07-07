17e9199c8d75999eaa4e304843b086f225b2456b
Рогожников Алексей Витальевич <rogozhnikov.aleksei@otr.ru>
2021-07-07 15:10:14 +0500

    Example Lombok

--- src/main/java/com/fitness/model/Food.java
+++ src/main/java/com/fitness/model/Food.java
@@ -1,77 +1,36 @@
 package com.fitness.model;
 
+import lombok.AccessLevel;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
-import javax.persistence.Entity;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 @Entity
 @Table(name = "food")
 @NoArgsConstructor
-public class Food implements Serializable {
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Food {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+    @SequenceGenerator(name = "foodSEQ", sequenceName = "foodSEQ", allocationSize = 1)
+    @GeneratedValue(generator = "foodSEQ", strategy = GenerationType.SEQUENCE)
     @Column(name = "fdc_id")
-    private Long fdc_id;
-    @Column(name = "data_type")
-    private String data_type = "";
+    Long id;
 
+    @Column(name = "data_type")
+    String dataType;
 
     @Column(name = "description")
-    private String description;
+    String description;
+
     @Column(name = "food_category_id")
-    private Long food_category_id;
+    Long foodCategoryId;
+
     @Column(name = "publication_date")
-    private Date publication_date;
-
-
-    public Long getFdc_id() {
-        return fdc_id;
-    }
-
-    public void setFdc_id(Long fdc_id) {
-        this.fdc_id = fdc_id;
-    }
-
-    public String getData_type() {
-        return data_type;
-    }
-
-    public void setData_type(String data_type) {
-        if (data_type != null) this.data_type = data_type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        if (description != null) this.description = description;
-    }
-
-    public Long getFood_category_id() {
-        return food_category_id;
-    }
-
-    public void setFood_category_id(Long food_category_id) {
-        if (food_category_id != null)
-            this.food_category_id = food_category_id;
-    }
-
-    public Date getPublication_date() {
-        return publication_date;
-    }
-
-    public void setPublication_date(Date publication_date) {
-        if (publication_date != null)
-            this.publication_date = publication_date;
-    }
+    Date publicationDate;
 }
